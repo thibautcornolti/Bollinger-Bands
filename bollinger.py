d76d38946da6e5a084785f292daa84fd098b0d7f
Bastien <bastien.rinck@epitech.eu>
2018-02-07 15:56:28 +0100

    o

--- bollinger.py
+++ bollinger.py
@@ -3,36 +3,49 @@
 from sys import argv
 
 if len(argv) != 5:
-    raise Exception('Invalid argument number')
+    raise Exception("Invalid argument number")
 
-index = int(argv[4])
-period = int(argv[1])
-sd_coefficient = float(argv[2])
-file_stream = open(argv[3], "r")
+try:
+    index = int(argv[4])
+    period = int(argv[1])
+    sd_coefficient = float(argv[2])
+except:
+    raise Exception("Invalid argument value")
+try:
+    file_stream = open(argv[3], "r")
+except:
+    raise Exception("Invalid file")
 
 
 def average(arr):
-    return sum(arr) / len(arr)
+    return sum(arr) / len(arr) if (len(arr)) else 0
 
 
 def deviation(arr, avg):
-    dev = [
-        (v - avg) ** 2 for v in arr
-    ]
-    return (1 / period * sum(dev)) ** (1 / 2)
+    return (1 / period * sum([(v - avg) ** 2 for v in arr])) ** (1 / 2)
 
 
-values = [
-    float(line[:-1])
-    for line in file_stream.readlines()
-]
-values = values[index - period + 1: index + 1]
-deviation = deviation(values, average(values))
+def main():
+    values = [
+        float(line[:-1])
+        for line in file_stream.readlines()
+    ][index - period + 1: index + 1]
+    avg = average(values)
+    dev = deviation(values, avg)
 
-upper_band = average(values) + (deviation * sd_coefficient)
-lower_band = average(values) - (deviation * sd_coefficient)
+    upper_band = avg + (dev * sd_coefficient)
+    lower_band = avg - (dev * sd_coefficient)
 
-print("MA: %.2f" % average(values))
-print("SD: %.2f" % deviation)
-print("B+: %.2f" % upper_band)
-print("B-: %.2f" % lower_band)
+    print("INPUT")
+    print("Index:", index)
+    print("Period:", period)
+    print("SD_coef: %.2f" % sd_coefficient)
+    print("\nOUTPUT")
+    print("MA: %.2f" % avg)
+    print("SD: %.2f" % deviation)
+    print("B+: %.2f" % upper_band)
+    print("B-: %.2f" % lower_band)
+
+
+if __name__ == "__main__":
+    main()
